e692bab9c8494c0a1bdb5ffcadc2fe0baed63fb7
Javier Mendiara Cañardo <javier.mendiaracanardo@telefonica.com>
2020-12-11 16:51:34 +0100

    chore: linting to ignore lib folder

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 export const BOILERPLATE = true;
 
-import { EventEmitter} from 'events';
-
 export function hello(what: string): string {
   return `Hello ${what}`;
-  const plo = new EventEmitter();
-
 }
