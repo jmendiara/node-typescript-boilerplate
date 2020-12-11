2a9a15d0b67fc36271dea88b8d1c293106f0f8d8
Javier Mendiara Cañardo <javier.mendiaracanardo@telefonica.com>
2020-12-11 16:39:44 +0100

    chore: improvements

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-
 export const BOILERPLATE = true;
+
+import { EventEmitter} from 'events';
 
 export function hello(what: string): string {
   return `Hello ${what}`;
+  const plo = new EventEmitter();
+
 }
